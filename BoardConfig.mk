68399ea436e103e115e33b10fec6acbe74a25582
Shirayuki39 <lorddemecrius83@proton.me>
2024-08-29 09:59:00 +0800

    LG8n: Inherit configuration from common trees
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include the common OEM chipset BoardConfig.
+include device/tecno/pova4series-common/BoardConfigCommon.mk
+
 # Inherit the proprietary files
 include vendor/tecno/LG8n/BoardConfigVendor.mk
