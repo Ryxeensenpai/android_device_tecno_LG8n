0d87f286a06c013727f00a5c0267a0010d2e1df8
Megumin <nezukokawai27@gmail.com>
2025-04-19 16:48:41 +0000

    LG8n: ThePixelProject bringup
    Signed-off-by: Megumin <nezukokawai27@gmail.com>

--- aosp_LG8n.mk
+++ aosp_LG8n.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The PixelProject Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/LG8n/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ThePixelProject stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
-PRODUCT_NAME := lineage_LG8n
+PRODUCT_NAME := aosp_LG8n
 PRODUCT_DEVICE := LG8n
 PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
@@ -27,4 +27,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=LG8n \
     BuildFingerprint=TECNO/LG8n-GL/TECNO-LG8n:12/SP1A.210812.016/240531V1800:user/release-keys
 
-
+# Custom Flags
+CUSTOM_MAINTAINER := Roseweiss
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_CALL_RECORDING_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
